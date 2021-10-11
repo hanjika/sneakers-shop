da34d2658b4073f96ae8548aa4478a325fa60347
Hanna Masanjika <hanna.masanjika@gmail.com>
2021-10-11 14:18:47 +0200

    Basic html structure formed

---
+++ src/js/data.js
@@ -0,0 +1,24 @@
+export const SNEAKER = [
+    {
+        'id': 1,
+        'enlarged': '../src/images/image-product-1.jpg',
+        'thumbnail': '../src/images/image-product-1-thumbnail.jpg'
+    },
+    {
+        'id': 2,
+        'enlarged': '../src/images/image-product-2.jpg',
+        'thumbnail': '../src/images/image-product-2-thumbnail.jpg'
+    },
+    {
+        'id': 3,
+        'enlarged': '../src/images/image-product-3.jpg',
+        'thumbnail': '../src/images/image-product-3-thumbnail.jpg'
+    },
+    {
+        'id': 4,
+        'enlarged': '../src/images/image-product-4.jpg',
+        'thumbnail': '../src/images/image-product-4-thumbnail.jpg'
+    }
+]
+
+console.log(SNEAKER[0].id);
